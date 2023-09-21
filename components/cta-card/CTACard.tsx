4bf4a212fa110569f648cbebb08569f122ec8580
shokouhSkr <shokouhskr@gmail.com>
2023-09-22 00:58:31 +0330

    fixed contact page loading issue

--- components/cta-card/CTACard.tsx
+++ components/cta-card/CTACard.tsx
@@ -48,7 +48,7 @@ const CTACard = ({ dictionary, locale }: { dictionary: any; locale: string }) =>
   return (
     <div className="relative px-6 py-10 overflow-hidden rounded-md bg-slate-100">
       {/* OVERLAY */}
-      <div className="absolute inset-0 z-10 bg-gradient-to-br from-white/95 dark:from-gray-600/95 via-white/70 dark:via-gray-600/70 to-white/30 dark:to-gray-600/30" />
+      <div className="absolute inset-0 z-10 bg-gradient-to-br from-white/95 dark:from-neutral-600/95 via-white/70 dark:via-neutral-600/70 to-white/30 dark:to-neutral-600/30" />
 
       {/* IMAGE */}
       <Image fill alt="CTA Card Image" className="object-cover object-center" src={ctaCardImg} />
--- components/post/PostForm.tsx
+++ components/post/PostForm.tsx
@@ -2,7 +2,7 @@
 
 import { FormEvent, useState } from "react";
 
-const PostForm = ({ dictionary }: { dictionary: any }) => {
+const PostForm = ({ dictionary }: { dictionary?: any }) => {
   const [formData, setFormData] = useState({ name: "", email: "", message: "" });
   const [isHandling, setIsHandling] = useState(false);
 
@@ -37,7 +37,7 @@ const PostForm = ({ dictionary }: { dictionary: any }) => {
         type="text"
         value={formData.name}
         onChange={(e) => setFormData({ ...formData, name: e.target.value })}
-        placeholder={dictionary.contact.form.name}
+        placeholder={dictionary?.contact?.form?.name ?? ""}
         className="p-4 shadow rounded-md focus:outline-none focus:ring-1 focus:ring-neutral-400 placeholder:text-sm sm:placeholder:text-base placeholder:italic "
       />
 
@@ -46,7 +46,7 @@ const PostForm = ({ dictionary }: { dictionary: any }) => {
         type="email"
         value={formData.email}
         onChange={(e) => setFormData({ ...formData, email: e.target.value })}
-        placeholder={dictionary.contact.form.email}
+        placeholder={dictionary?.contact?.form?.email ?? ""}
         className="p-4 shadow rounded-md focus:outline-none focus:ring-1 focus:ring-neutral-400 placeholder:text-sm sm:placeholder:text-base placeholder:italic "
       />
 
@@ -54,7 +54,7 @@ const PostForm = ({ dictionary }: { dictionary: any }) => {
       <textarea
         value={formData.message}
         onChange={(e) => setFormData({ ...formData, message: e.target.value })}
-        placeholder={dictionary.contact.form.message}
+        placeholder={dictionary?.contact?.form?.message ?? ""}
         cols={30}
         rows={10}
         className="resize-none p-4 shadow rounded-md focus:outline-none focus:ring-1 focus:ring-neutral-400 placeholder:text-sm placeholder:italic sm:placeholder:text-base"
@@ -67,7 +67,7 @@ const PostForm = ({ dictionary }: { dictionary: any }) => {
           onClick={submitHandler}
           className="bg-green-700 text-neutral-100 w-full md:inline-block py-2 rounded-md hover:bg-green-800 active:bg-green-800 active:scale-[.98] duration-200 md:w-28"
         >
-          {!isHandling ? `${dictionary.buttons.send}` : `${dictionary.buttons.sending}`}
+          {!isHandling ? `${dictionary?.buttons?.send}` : `${dictionary?.buttons?.sending}`}
         </button>
       </div>
     </form>
